d56a335b365b13099fc5ba68ef32a723319cf3df
Francesco Silvestri <frasil@users.noreply.github.com>
2024-12-21 16:47:33 +0100

    One year more in publication list

--- update-papers.py
+++ update-papers.py
@@ -71,7 +71,7 @@ def format_year(pubs, year):
 def format_all(pubs):
     formatted = [
         format_year(pubs, str(year))
-        for year in reversed(range(2020, 2024))
+        for year in reversed(range(2020, 2026))
     ]
     return "\n".join(formatted)
 
